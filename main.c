a7bf61f9729fd88d4de67d6eb57ee43795f8a8c6
ace <alexmatamoros10@gmail.com>
2023-11-02 12:36:41 -0700

    removed line in main.c

--- main.c
+++ main.c
@@ -5,7 +5,6 @@ int main() {
     printf("Hello World\n");
     int* arr = (int*) memo(5 * sizeof(int));
     char* string = (char*) memo(35000 * sizeof(char));
-    //char* string1 = (char*) memo(35000 * sizeof(char));
     char* string1 = (char*) memo(63192);
     string1[63191] = 'd';
     printf("string1[63278] = %c\n", string1[63191]);
